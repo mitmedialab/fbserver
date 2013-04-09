9c52f6c0f77c7d3bacfb8a5e7679496aed52ff1f
natematias <natematias@gmail.com>
2013-04-08 22:44:54 -0400

    added dropdown topbar, styles for long scroll content

--- app/assets/javascripts/followbias.js
+++ app/assets/javascripts/followbias.js
@@ -5,17 +5,19 @@ var FollowBias = Backbone.View.extend({
   
   initialize: function(){
     this.render_count = 0;
+    this.topbar_down = false
     this.canvas = Raphael("background", "100%", "100%");
     this.render();
     $(window).bind("resize.app", _.bind(this.render, this));
+    $(window).bind("scroll", _.bind(this.scroll, this));
   },
 
   render: function(){
     if(this.render_count >=3){this.render_count=0;}
 
-    men_percent = 0.25;
+    men_percent = 0.35;
     women_percent = 0.45;
-    bots_percent = 0.30;
+    bots_percent = 0.20;
 
     bots_start = 180.0 + (180 - (bots_percent * 360.0) ) / 2.0;
     bots_end = bots_start + (bots_percent * 360.0);
@@ -42,6 +44,7 @@ var FollowBias = Backbone.View.extend({
     cx = window.innerWidth/2.0
     cy = window.innerHeight/2.0;
 
+
 /*		this.sector(this.canvas, cx, cy, 900, -10, 75, {fill: "rgb(211,113,136)", "stroke-width": 0});
 		this.sector(this.canvas, cx, cy, 900, 74, 191, {fill: "rgb(105,127,158)", "stroke-width": 0});
 		this.sector(this.canvas, cx, cy, 900, 190, 351, {fill: "rgb(91,91,91)", "stroke-width": 0});*/
@@ -55,6 +58,8 @@ var FollowBias = Backbone.View.extend({
     glasses.css("width", glasses_radius+ "px");
     glasses.css("left", cx - parseInt(glasses_radius/2.0) + "px");
     glasses.css("top", cy - parseInt(glasses.height()/2.0) + "px");
+
+    this.circle_center = cy + glasses.height()/2.0;
 
     chapter_one = $("#chapter_one")
     chapter_one.css("margin-top", "-" + parseInt(pie_radius * 0.8) + "px");
@@ -74,7 +79,9 @@ var FollowBias = Backbone.View.extend({
     women_percent_label = $("#women_percent")
 
     men_percent_label.html(parseInt(men_percent * 100) + "%");
+    $("#topbar_men").html(parseInt(men_percent * 100) + "%");
     women_percent_label.html(parseInt(women_percent * 100) + "%");
+    $("#topbar_women").html(parseInt(women_percent * 100) + "%");
 
     men_percent_label.css("left", parseInt(cx - pie_radius/1.35) + "px")
     men_percent_label.css("font-size", parseInt((52/240)*pie_radius) + "px");
@@ -95,9 +102,27 @@ var FollowBias = Backbone.View.extend({
         y1 = cy + r * Math.sin(-startAngle * rad),
         y2 = cy + r * Math.sin(-endAngle * rad);
     return paper.path(["M", cx, cy, "L", x1, y1, "A", r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"]).attr(params);
+  },
+  
+  drop_menu: function(){
+    d3.select("#topbar").transition().ease("cubic").duration(200).style("margin-top", "0px");
+  },
+  
+  raise_menu: function(){
+    d3.select("#topbar").transition().ease("cubic").duration(200).style("margin-top", "-65px");
+  },
+ 
+  scroll: function(){
+    background = $("#background")
+    topbar = $("#topbar")
+    if(this.topbar_down == false &&  $(window).scrollTop() > this.circle_center && parseInt(topbar.css("margin-top") ) < 0){
+      this.topbar_down = true;
+      this.drop_menu();
+    }else if( this.topbar_down == true && $(window).scrollTop() < this.circle_center && parseInt(topbar.css("margin-top"))==0){
+      this.raise_menu();
+      this.topbar_down = false
+    }
   }
-
-
 
 });
 
