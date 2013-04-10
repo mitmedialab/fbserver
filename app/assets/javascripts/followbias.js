597f7e61a93fb34e995fb9865b9a9341e4788d28
natematias <natematias@gmail.com>
2013-04-09 23:39:00 -0400

    added waiting spinner. Linked FollowBias#main to actual json data. Added stub interface elements for fixing gender judgments

--- app/assets/javascripts/followbias.js
+++ app/assets/javascripts/followbias.js
@@ -6,6 +6,7 @@ var FollowBias = Backbone.View.extend({
   initialize: function(){
     this.render_count = 0;
     this.topbar_down = false
+    this.followbias_data = null;
     this.canvas = Raphael("background", "100%", "100%");
     this.render();
     $(window).bind("resize.app", _.bind(this.render, this));
@@ -13,11 +14,102 @@ var FollowBias = Backbone.View.extend({
   },
 
   render: function(){
-    if(this.render_count >=3){this.render_count=0;}
+    if(this.followbias_data == null){
+      this.render_base_circle();
+      this.start_spinner();
+      this.fetch_followbias();
+    }else{
+      this.render_glasses();
+    }
+  },
 
-    men_percent = 0.35;
-    women_percent = 0.45;
-    bots_percent = 0.20;
+  fetch_followbias: function(){
+    that = this;
+    jQuery.get("/followbias/show/" + followbias_screen_name + ".json", function(data){
+      if(data!=null){
+        that.followbias_data = data;
+        that.render();
+      }else{
+        window.setTimeout(function(){follow_bias.fetch_followbias()}, 15000)
+      }
+    }).error(function(){
+      window.setTimeout(function(){follow_bias.fetch_followbias()}, 15000)
+    });
+
+  },
+
+  generate_dimensions: function(){
+    pie_rad_width = (window.innerWidth * 0.5) * 0.75
+    pie_rad_height = (window.innerHeight * 0.5) * 0.75;
+    if(pie_rad_width < pie_rad_height){
+      pie_radius = pie_rad_width;
+    }else{
+      pie_radius = pie_rad_height;
+    }
+
+    cx = window.innerWidth/2.0
+    cy = window.innerHeight/2.0;
+    glasses_radius = parseInt(pie_radius*2.5);
+
+    return {
+      cx:cx,
+      cy:cy,
+      pie_radius:pie_radius,
+      glasses_radius:glasses_radius
+    }
+  },
+
+  render_base_circle: function(){
+    d = this.generate_dimensions();
+
+    this.canvas.clear();
+
+    glasses_radius = parseInt(d.pie_radius*2.5);
+
+
+    glasses = $("#main_glasses");
+    glasses.css("width", glasses_radius+ "px");
+    glasses.css("left", d.cx - parseInt(glasses_radius/2.0) + "px");
+    glasses.css("top", d.cy - parseInt(glasses.height()/2.0) + "px");
+
+    left_eye = this.canvas.rect(cx - d.pie_radius/1.2, cy - glasses.height() /4.5 , glasses.width()/4,glasses.height()/2.0);
+    left_eye.attr("fill", "rgb(105,127,158)");
+    left_eye.attr("opacity", "0.7");
+    left_eye.attr("stroke-width", "0");
+    
+    right_eye = this.canvas.rect(cx + d.pie_radius/4.0, cy - glasses.height() /4.5 , glasses.width()/4,glasses.height()/2.0);
+    right_eye.attr("fill", "rgb(211,113,136)");
+    right_eye.attr("opacity", "0.7");
+    right_eye.attr("stroke-width", "0");
+
+
+    this.circle_center = d.cy + glasses.height()/2.0;
+
+    chapter_one = $("#chapter_one")
+    chapter_one.css("margin-top", "-" + parseInt(d.pie_radius * 0.8) + "px");
+    chapter_one.css("font-size", parseInt((18/240)*d.pie_radius) + "px");
+    chapter_one.css("line-height", parseInt((18/240)*d.pie_radius) + "px");
+    chapter_one.css("margin-bottom", parseInt((30/240)*d.pie_radius) + "px");
+
+    $("#personal_see").css("font-size", parseInt((32/240)*d.pie_radius) + "px");
+    $("#personal_see").css("line-height", parseInt((32/240)*d.pie_radius) + "px");
+
+    circle = this.canvas.circle(d.cx, d.cy, d.pie_radius);
+    circle.attr("fill", "#fff");
+    circle.attr("fill-opacity", "0.20");
+    circle.attr("stroke-width", "0");
+  },
+
+  render_glasses: function(){
+    if(this.render_count >=3){this.render_count=0;}
+  
+    men_percent = parseFloat(this.followbias_data.male) / parseFloat(this.followbias_data.total_following);
+    women_percent = parseFloat(this.followbias_data.female) / parseFloat(this.followbias_data.total_following);
+    bots_percent = 1.0 - (men_percent + women_percent);
+ 
+    /*men_percent = 0.32;
+    women_percent = 0.22;
+    bots_percent = 0.45;*/
 
     bots_start = 180.0 + (180 - (bots_percent * 360.0) ) / 2.0;
     bots_end = bots_start + (bots_percent * 360.0);
@@ -33,47 +125,17 @@ var FollowBias = Backbone.View.extend({
       return null;
     }
 
-    pie_rad_width = (window.innerWidth * 0.5) * 0.75
-    pie_rad_height = (window.innerHeight * 0.5) * 0.75;
-    if(pie_rad_width < pie_rad_height){
-      pie_radius = pie_rad_width;
-    }else{
-      pie_radius = pie_rad_height;
-    }
+    // Generate Dimensions
+    d = this.generate_dimensions();
 
-    cx = window.innerWidth/2.0
-    cy = window.innerHeight/2.0;
+    this.render_base_circle();
 
-
-/*		this.sector(this.canvas, cx, cy, 900, -10, 75, {fill: "rgb(211,113,136)", "stroke-width": 0});
-		this.sector(this.canvas, cx, cy, 900, 74, 191, {fill: "rgb(105,127,158)", "stroke-width": 0});
-		this.sector(this.canvas, cx, cy, 900, 190, 351, {fill: "rgb(91,91,91)", "stroke-width": 0});*/
-
-		this.sector(this.canvas, cx, cy, 900, bots_end, women_start, {fill: "rgb(211,113,136)", "stroke-width": 0});
-		this.sector(this.canvas, cx, cy, 900, men_end, men_start, {fill: "rgb(105,127,158)", "stroke-width": 0});
-		this.sector(this.canvas, cx, cy, 900, bots_start, bots_end, {fill: "rgb(91,91,91)", "stroke-width": 0});
-
-    glasses_radius = parseInt(pie_radius*2.5);
-    glasses = $("#main_glasses");
-    glasses.css("width", glasses_radius+ "px");
-    glasses.css("left", cx - parseInt(glasses_radius/2.0) + "px");
-    glasses.css("top", cy - parseInt(glasses.height()/2.0) + "px");
-
-    this.circle_center = cy + glasses.height()/2.0;
-
-    chapter_one = $("#chapter_one")
-    chapter_one.css("margin-top", "-" + parseInt(pie_radius * 0.8) + "px");
-    chapter_one.css("font-size", parseInt((18/240)*pie_radius) + "px");
-    chapter_one.css("line-height", parseInt((18/240)*pie_radius) + "px");
-    chapter_one.css("margin-bottom", parseInt((30/240)*pie_radius) + "px");
-
-    $("#personal_see").css("font-size", parseInt((32/240)*pie_radius) + "px");
-    $("#personal_see").css("line-height", parseInt((32/240)*pie_radius) + "px");
-
-    circle = this.canvas.circle(cx, cy, pie_radius);
-    circle.attr("fill", "#fff");
-    circle.attr("fill-opacity", "0.20");
-    circle.attr("stroke-width", "0");
+		pink = this.sector(this.canvas, d.cx, d.cy, 900, bots_end, women_start, {fill: "rgb(211,113,136)", "stroke-width": 0});
+    pink.toBack();
+		blue = this.sector(this.canvas, d.cx, d.cy, 900, men_end, men_start, {fill: "rgb(105,127,158)", "stroke-width": 0});
+    blue.toBack();
+		grey = this.sector(this.canvas, d.cx, d.cy, 900, bots_start, bots_end, {fill: "rgb(91,91,91)", "stroke-width": 0});
+    grey.toBack();
 
     men_percent_label = $("#men_percent")
     women_percent_label = $("#women_percent")
@@ -83,16 +145,32 @@ var FollowBias = Backbone.View.extend({
     women_percent_label.html(parseInt(women_percent * 100) + "%");
     $("#topbar_women").html(parseInt(women_percent * 100) + "%");
 
-    men_percent_label.css("left", parseInt(cx - pie_radius/1.35) + "px")
-    men_percent_label.css("font-size", parseInt((52/240)*pie_radius) + "px");
-    men_percent_label.css("line-height", parseInt((52/240)*pie_radius) + "px");
-    men_percent_label.css("top", parseInt(cy - men_percent_label.height()/3) + "px")
+    men_percent_label.css("left", parseInt(d.cx - d.pie_radius/1.35) + "px")
+    men_percent_label.css("font-size", parseInt((52/240)*d.pie_radius) + "px");
+    men_percent_label.css("line-height", parseInt((52/240)*d.pie_radius) + "px");
+    men_percent_label.css("top", parseInt(d.cy - men_percent_label.height()/3) + "px")
 
-    women_percent_label.css("left", parseInt(cx + pie_radius/3.0) + "px")
-    women_percent_label.css("font-size", parseInt((52/240)*pie_radius) + "px");
-    women_percent_label.css("line-height", parseInt((52/240)*pie_radius) + "px");
-    women_percent_label.css("top", parseInt(cy - women_percent_label.height()/3) + "px")
+    women_percent_label.css("left", parseInt(d.cx + d.pie_radius/3.0) + "px")
+    women_percent_label.css("font-size", parseInt((52/240)*d.pie_radius) + "px");
+    women_percent_label.css("line-height", parseInt((52/240)*d.pie_radius) + "px");
+    women_percent_label.css("top", parseInt(d.cy - women_percent_label.height()/3) + "px")
+   
+  },
 
+  start_spinner: function(){
+    if(this.spinner!=null){
+      this.spinner.remove();
+      this.spinner = null;
+    }
+
+    d = this.generate_dimensions();
+    this.spinner = this.sector(this.canvas, d.cx, d.cy, 900, 0, 20, {fill: "#ffff00", "stroke-width": 0});
+    this.spinner.attr("opacity", "0.5");
+    this.spinner.toBack();
+    a = Raphael.animation({transform: "r360" + "," + d.cx + "," + d.cy}, 10000);
+    this.spinner.animate(a.repeat(Infinity));
+  },
+  stop_spinner: function(){
   },
 
   sector: function(paper, cx, cy, r, startAngle, endAngle, params) {
--- app/controllers/followbias_controller.rb
+++ app/controllers/followbias_controller.rb
@@ -52,7 +52,7 @@ class FollowbiasController < ApplicationController
 
   def main
     @current_user ||= User.find(session[:user_id]) if session[:user_id]  
-    #redirect_to "/" and return if @current_user.nil?
+    redirect_to "/" and return if @current_user.nil?
     render :layout=>"main"
   end
 
