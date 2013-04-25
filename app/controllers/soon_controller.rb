714fd67f384561715e23104dadda01b061143f1d
natematias <natematias@gmail.com>
2013-04-25 00:34:39 -0400

    added simple script to queue up all tracked accounts for queue processing

--- app/controllers/soon_controller.rb
+++ app/controllers/soon_controller.rb
@@ -1,8 +1,29 @@
+# -*- encoding : utf-8 -*-
+require "resque"
+#require 'resque/plugins/lock'
+require 'resque-lock-timeout'
+
+class ProcessUserFriends
+  extend Resque::Plugins::LockTimeout
+  @queue = :fetchfriends
+  @lock_timeout = 3600
+  def self.perform(authdata)
+  end
+end
+
 class SoonController < ApplicationController
 
   def index
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     redirect_to "/" and return if @current_user.nil?
+
+    #we calculate the FollowBias for all users, including the control group
+    authdata = {:consumer_key => ENV['TWITTER_CONSUMER_KEY'],
+                :consumer_secret => ENV['TWITTER_CONSUMER_SECRET'],
+                :oauth_token => @current_user['twitter_token'],
+                :oauth_token_secret => @current_user['twitter_secret']}
+    Resque.enqueue(ProcessUserFriends, authdata)
+
     render :layout=>"main"
   end
 
---
+++ utilities/update_user_followbias.rb
@@ -0,0 +1,40 @@
+require File.join(File.dirname(__FILE__), '..', 'config', 'environment.rb')
+require File.join(File.dirname(__FILE__), '..', 'app', 'workers', 'workers.rb')
+require 'twitter'
+require 'csv'
+require 'resque'
+require 'resque-lock-timeout'
+
+class ProcessUserFriends
+  extend Resque::Plugins::LockTimeout
+  @queue = :fetchfriends
+  @lock_timeout = 30
+  def self.perform(authdata)
+  end
+end
+
+user_counter = 0 
+users = User.where("twitter_token IS NOT NULL and twitter_secret IS NOT NULL")
+
+whitelist = User.where("treatment='test' OR treatment='ctl' or treatment='exp'")
+
+whitelist.each do |row|
+  screen_name = row.screen_name
+
+  if(row.twitter_token.nil? or row.twitter_secret.nil?)
+    user_counter = 0 if(user_counter >= users.size)
+    user = users[user_counter]
+    user_counter += 1
+  else
+    user = row
+  end
+  
+  authdata = {:consumer_key => ENV["TWITTER_CONSUMER_KEY"],
+              :consumer_secret => ENV["TWITTER_CONSUMER_SECRET"],
+              :oauth_token => user.twitter_token,
+              :oauth_token_secret => user.twitter_secret,
+              :api_user => user.screen_name,
+              :followbias_user => screen_name}
+  Resque.enqueue(ProcessUserFriends, authdata)
+  puts authdata
+end
