8541c9cc679761b8c7baf93ea12aa5f31f395800
J. Nathan Matias <natematias@gmail.com>
2013-12-11 05:20:45 +0000

    updated account_gender_judgment relationship to belongs_to

--- app/models/account_gender_judgment.rb
+++ app/models/account_gender_judgment.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 class AccountGenderJudgment < ActiveRecord::Base
   attr_accessible :account_id, :user_id, :gender
-  has_one :account
-  has_one :user
+  belongs_to :account
+  belongs_to :user
   validates :account_id, :presence => true
   validates :user_id, :presence => true
 end
